192474c24484cd593617e84a3884ffeebcd9799c
123 <test@example.com>
2024-02-22 23:31:31 +0300

    added variables to main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,12 @@ provider "aws" {
 
 resource "aws_instance" "vm1" {
   count         = 2
-  ami           = "ami-0fb653ca2d3203ac1"
+  ami           = "var.ami"
   instance_type = "t2.micro"
+}
+
+variable "ami" {
+  type        = string
+  default     = "ami-0fb653ca2d3203ac1"
+  description = "AMI to use"
 }
