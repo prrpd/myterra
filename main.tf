24ae0985ba0562977eb72af5579d3132faff7c77
123 <test@example.com>
2024-02-22 23:43:16 +0300

    moved var to variables.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,4 @@ resource "aws_instance" "vm1" {
   instance_type = "t2.micro"
 }
 
-variable "ami" {
-  type        = string
-  default     = "ami-0fb653ca2d3203ac1"
-  description = "AMI to use"
-}
+
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "ami" {
+  type        = string
+  default     = "ami-0fb653ca2d3203ac1"
+  description = "AMI to use"
+}
