bbd49e7849aa584164f572826707edba9c1dd06b
plux <test@example.com>
2024-02-24 13:41:21 +0300

    added subnet, changed output

--- myTest2/main.tf
+++ myTest2/main.tf
@@ -8,7 +8,11 @@ provider "aws" {
   }
 }
 
-data "aws_vpc" "vpc_data" {}
+data "aws_vpc" "vpc_data" {
+  tags = {
+    Name = "dev"
+  }
+}
 data "aws_region" "region_data" {}
 
 resource "aws_vpc" "vpc_dev" {
@@ -17,4 +21,9 @@ resource "aws_vpc" "vpc_dev" {
     Name   = "dev"
     Region = data.aws_region.region_data.description
   }
+}
+
+resource "aws_subnet" "subnet_dev1" {
+  vpc_id     = aws_vpc.vpc_dev.id
+  cidr_block = "10.10.1.0/28"
 }
--- myTest2/outputs.tf
+++ myTest2/outputs.tf
@@ -1,4 +1,13 @@
-output "vpc" {
-  value       = aws_vpc.vpc_dev.instance_tenancy
+output "vpc_id" {
+  value       = aws_vpc.vpc_dev.id
+  description = "testing output"
+}
+
+output "vpc_cidr" {
+  value       = aws_vpc.vpc_dev.cidr_block
+  description = "testing output"
+}
+output "subnet_id" {
+  value       = aws_subnet.subnet_dev1.id
   description = "testing output"
 }
