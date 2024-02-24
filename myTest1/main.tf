1b8e047e4f77a379bb930674b345c9d830b6a857
plux <test@example.com>
2024-02-24 14:24:10 +0300

    added data source for AMI, configured to use either AMI from var or from data source

--- myTest1/main.tf
+++ myTest1/main.tf
@@ -7,9 +7,18 @@ provider "aws" {
     }
   }
 }
+
+data "aws_ami" "ami_ubuntu_22_04_latest" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
 resource "aws_instance" "vm1" {
   count                  = var.instanceCount
-  ami                    = var.ami
+  ami                    = var.ami != "" ? var.ami : data.aws_ami.ami_ubuntu_22_04_latest.id #setting ami id value from either var or from data source - https://www.linkedin.com/pulse/datasource-default-value-terraform-variable-mohd-fawaz-akhtar
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webserversg.id]
   user_data              = file("apache.sh")
