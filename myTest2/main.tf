8e69af92a48329a020d84f443e84e97f6308fe12
plux <test@example.com>
2024-02-24 13:15:58 +0300

    configured vpc creation and tags

--- myTest2/main.tf
+++ myTest2/main.tf
@@ -7,30 +7,14 @@ provider "aws" {
     }
   }
 }
-resource "aws_instance" "vm1" {
-  #count                  = var.instanceCount
-  ami                    = var.ami
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.webserversg.id]
-  user_data              = file("apache.sh")
-}
 
-resource "aws_security_group" "webserversg" {
-  dynamic "ingress" {
-    for_each = ["80", "443", "8080"]
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+data "aws_vpc" "vpc_data" {}
+data "aws_region" "region_data" {}
+
+resource "aws_vpc" "vpc_dev" {
+  cidr_block = "10.10.1.0/24"
+  tags = {
+    Name   = "dev"
+    Region = data.aws_region.region_data.description
   }
 }
-
-
--- myTest2/outputs.tf
+++ myTest2/outputs.tf
@@ -1,20 +1,4 @@
-output "instance_arn" {
-  value       = aws_instance.vm1.arn
+output "vpc" {
+  value       = aws_vpc.vpc_dev.instance_tenancy
   description = "testing output"
-}
-
-output "instance_id" {
-  value = aws_instance.vm1.id
-}
-
-output "private_ip" {
-  value = aws_instance.vm1.private_ip
-}
-
-output "public_ip" {
-  value = aws_instance.vm1.public_ip
-}
-
-output "security_group_id" {
-  value = aws_security_group.webserversg.id
 }
