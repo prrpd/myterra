ddce5e0b69d64ce5a453e015dd456354a748d255
plux <test@example.com>
2024-02-26 19:26:19 +0300

    changed apache, added private ip

--- myTest1/apache.sh
+++ myTest1/apache.sh
@@ -2,4 +2,7 @@
 sudo apt update -y
 sudo apt-get install apache2 -y
 sudo systemctl start apache2
-sudo bash -c 'echo Congratulations! on your first Webserver Installation > /var/www/html/index.html'
+myip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+sudo bash -c "echo Congratulations! on your first Webserver Installation > /var/www/html/index.html"
+sudo bash -c "echo $myip >> /var/www/html/index.html"
+sudo bash -c "echo V 1 >> /var/www/html/index.html"
